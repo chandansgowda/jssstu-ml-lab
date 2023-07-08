af08d329170f46da212f3d6d40bed8c5136403ac
chandansgowda <chandansuresh007@gmail.com>
2023-07-08 14:41:27 +0530

    Best First Search

--- 1/b.py
+++ 1/b.py
@@ -1,36 +1,54 @@
-def best_first_search(graph,start,goal):
+def best_first_search(graph,start,goal,heuristic, path=[]):
     open_list = [(0,start)]
     closed_list = set()
+    closed_list.add(start)
+
     while open_list:
-        open_list.sort(reverse=True)
+        open_list.sort(key = lambda x: heuristic[x[1]], reverse=True)
         cost, node = open_list.pop()
-        
-        if node in closed_list:
-            continue
+        path.append(node)
 
         if node==goal:
-            return cost
+            return cost, path
 
-        closed_list.add((cost,node))
+        closed_list.add(node)
         for neighbour, neighbour_cost in graph[node]:
-            if neighbour not in closed_list and neighbour not in closed_list:
-                open_list.append((neighbour_cost+cost, neighbour))
+            if neighbour not in closed_list:
+                closed_list.add(node)
+                open_list.append((cost+neighbour_cost, neighbour))
 
     return None
 
 
 graph = {
-    'A': [('B', 2), ('C', 4)],
-    'B': [('C', 1)],
-    'C': []
+    'A': [('B', 11), ('C', 14), ('D',7)],
+    'B': [('A', 11), ('E', 15)],
+    'C': [('A', 14), ('E', 8), ('D',18), ('F',10)],
+    'D': [('A', 7), ('F', 25), ('C',18)],
+    'E': [('B', 15), ('C', 8), ('H',9)],
+    'F': [('G', 20), ('C', 10), ('D',25)],
+    'G': [],
+    'H': [('E',9), ('G',10)]
 }
 
-start = input("Start node: ")
-goal = input("Goal node: ")
+start = 'A'
+goal = 'G'
 
-result = best_first_search(graph, start, goal)
+heuristic = {
+    'A': 40,
+    'B': 32,
+    'C': 25,
+    'D': 35,
+    'E': 19,
+    'F': 17,
+    'G': 0,
+    'H': 10
+}
+
+result = best_first_search(graph, start, goal, heuristic)
 
 if result:
-    print(f"The minimum cost from {start} to {goal} is {result}")
+    print(f"Minimum cost path from {start} to {goal} is {result[1]}")
+    print(f"Cost: {result[0]}")
 else:
     print(f"No path from {start} to {goal}")
