04606f50bd218c1522fda768ea08c8640595fc51
chandansgowda <chandansuresh007@gmail.com>
2023-07-10 09:29:46 +0530

    Visualization Programs updated

--- 2/a.py
+++ 2/a.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 
 dataset = pd.read_csv('./ToyotaCorolla.csv')
--- 4/a.py
+++ 4/a.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
 data=pd.read_csv('./ToyotaCorolla.csv')
